9810c393f1031201808f174da075acad4a220789
Kamaljit Grewal <kamal17@my.yorku.ca>
2026-03-16 13:29:43 -0400

    improvments to mobile layout

--- script.js
+++ script.js
@@ -74,3 +74,9 @@ toggle.addEventListener("click", () => {
 
 });
 
+const button = document.getElementById("mobile-menu-button");
+const menu = document.getElementById("mobile-menu");
+
+button.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
